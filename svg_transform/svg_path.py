b2fc33ce607870834065dd2cc331ffcb8ec47ace
Josef Ondřej <josef.ondrej@tipsport.cz>
2023-05-23 09:31:48 +0200

    Fix svg_path

--- svg_transform/svg_path.py
+++ svg_transform/svg_path.py
@@ -1,4 +1,75 @@
-from typing import Callable, Tuple
+from typing import Callable, Tuple, List, Optional
+
+COMMAND_TYPES = 'MmLlCcSsQqTtAaZzHhVv'
+
+
+class PathCommand:
+    def __init__(self, command_type: str, coordinates: Optional[List[float]] = None):
+        if command_type not in COMMAND_TYPES and len(command_type) != 1:
+            raise ValueError(f'Invalid command type: {command_type}')
+
+        self._command_type = command_type
+        self._coordinates = coordinates if coordinates is not None else list()
+
+    def transform(self, transformation: Callable[[Tuple[int, int]], Tuple[int, int]]):
+        if self._command_type in ['M', 'L', 'C', 'S', 'Q', 'T']:
+            transformed_coordinates = [
+                transformation((self._coordinates[i], self._coordinates[i + 1])) for i in
+                range(0, len(self._coordinates), 2)]
+            self._coordinates = [coordinate for point in transformed_coordinates for coordinate in point]
+        elif self._command_type in ['H']:
+            self._coordinates = [
+                transformation((self._coordinates[i], 0)
+                               )[0] for i in range(len(self._coordinates))]
+        elif self._command_type in ['V']:
+            self._coordinates = [
+                transformation((0, self._coordinates[i]))[1] for i in range(len(self._coordinates))]
+        elif self._command_type in ['A', 'a']:
+            raise NotImplementedError('Arcs are not implemented yet')
+        elif self._command_type in ['m', 'l', 'c', 's', 'q', 't', 'h', 'v']:
+            raise NotImplementedError('Relative commands are not implemented yet')
+        elif self._command_type in ['Z', 'z']:
+            pass
+
+    def __str__(self):
+        return self._command_type + ' '.join([str(coordinate) for coordinate in self._coordinates])
+
+    def __repr__(self):
+        return f'PathCommand({self._command_type}, {self._coordinates})'
+
+
+def parse_coordinates(coordinates: str) -> List[float]:
+    """
+    Parses a string of coordinates into a list of floats.
+    :param coordinates: String of coordinates
+    :return: List of floats
+    """
+    if coordinates.strip() == '':
+        return list()
+    return [float(coordinate.strip()) for coordinate in coordinates.strip().split(' ')]
+
+
+def parse_commands(path_command_string: str) -> List[PathCommand]:
+    """
+    Parses a path command string into a list of PathCommands.
+    :param path_command_string: Raw SVG path command string
+    :return: List of PathCommands
+    """
+    commands = list()
+    current_command = ''
+    current_coordinates = ''
+    for char in path_command_string:
+        if char in COMMAND_TYPES:
+            if current_command:
+                command = PathCommand(current_command, parse_coordinates(current_coordinates))
+                commands.append(command)
+            current_command = char
+            current_coordinates = ''
+        else:
+            current_coordinates += char
+    command = PathCommand(current_command, parse_coordinates(current_coordinates))
+    commands.append(command)
+    return commands
 
 
 def transform_path_command_string(path_command_string: str,
@@ -9,4 +80,7 @@ def transform_path_command_string(path_command_string: str,
     :param transformation: Transformation that maps points from R2 to R2
     :return: Transformed path command string
     """
-    raise NotImplemented('TODO: Implement this function')
+    commands = parse_commands(path_command_string)
+    for command in commands:
+        command.transform(transformation)
+    return ''.join([str(command) for command in commands])
--- tests/test_svg.py
+++ tests/test_svg.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pytest
 
@@ -17,13 +18,26 @@ def cross():
     return SVG.from_string(svg_string)
 
 
-def test_transform(cross: SVG):
-    no_transform = lambda x: x
-    cross.transform(
+@pytest.fixture
+def cross_abs():
+    """
+    SVG with a cross in the middle, absolute coordinates
+    """
+    svg_string = """
+    <svg class="icon  icon--plus" viewBox="0 0 5 5" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2 1 H3 V2 H4 V3 H3 V4 H2 V3 H1 V2 H2 Z" />
+    </svg>
+    """
+    return SVG.from_string(svg_string)
+
+
+def test_empty_transform(cross_abs: SVG):
+    no_transform = lambda x: (x[0], x[1])
+    cross_abs.transform(
         transformation=no_transform,
         inplace=True
     )
-    transformed_array = np.array(cross.to_pil_image(scale=1))
+    transformed_array = np.array(cross_abs.to_pil_image(scale=1))
     expected_transformed_array = [
         [0, 0, 0, 0, 0],
         [0, 0, 255, 0, 0],
@@ -33,3 +47,18 @@ def test_transform(cross: SVG):
     ]
 
     transformed_array.T[-1].tolist() == expected_transformed_array
+
+
+def test_shift_right_down(cross_abs: SVG):
+    shift_right_down = lambda x: (
+        x[0] + 1,
+        x[1] + 1
+    )
+    cross_abs.transform(
+        transformation=shift_right_down,
+        inplace=True
+    )
+    transformed_array = np.array(cross_abs.to_pil_image(scale=1))
+    print(transformed_array.T[-1])
+    plt.imshow(transformed_array.T[-1])
+    plt.show()
