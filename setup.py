df7bacfe05adb33bf8b79da44bf649c88d159932
Josef Ondrej <josef.ondrej@outlook.com>
2023-05-23 12:06:19 +0200

    Bump minor version from 0.2.0 to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='svg-transform',
-    version='0.1.0',
+    version='0.3.0',
     author='Josef Ondrej',
     author_email='josef.ondrej@outlook.com',
     description='Lightweight library for arbitrary geometric SVG transformations',
