79aec30fdfde3593c4cce659d400325fe599a74a
Ondrej Josef <Josef.Ondrej@tgw-group.com>
2024-07-19 09:08:12 +0200

    Update sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -15,14 +15,13 @@ release = '0.3.4'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
+autoapi_dirs = ["../"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
